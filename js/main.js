b310cbecdb27d991eccd1678f373ea6deed71b72
idalizakaria <57689415+idalizakaria@users.noreply.github.com>
2020-05-08 10:48:08 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var fruits = ['Mangue','Raisin','Figue','Kiwi'];
+document.getElementById("first").innerHTML=fruits;
+var fruits = ['Mangue','Raisin','Kiwi','Figue'];
+
+function isExist() {
+    var fruitAchter = document.getElementById("fruit").value;
+    var achat = false;
+    for (index = 0 ; index < fruits.length; index++ ){
+    
+        if (fruitAchter.toUpperCase() == fruits[index].toUpperCase()){
+            console.log("fruits "+fruits[index]);
+            console.log("fruitAchter "+fruitAchter);
+            
+            fruits.splice(index, 1);
+            
+            achat=true;
+        }
+    }
+
+    if (achat==true){
+        alert("Ok!");
+
+    }else {
+        alert("Indisponible");
+    }
+
+    document.getElementById("first").innerHTML=fruits;
+    
+
+}
